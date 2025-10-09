d664127f524fa2a8ba9928edd8337ce419657631
Aishwarya S <aishwaryaa0263@gmail.com>
2025-10-09 14:26:13 +0530

    Add files via upload

---
+++ DEMO API/index.js
@@ -0,0 +1,105 @@
+const myData = [
+    {
+        name:"Dharaneesh",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Naveen",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Priya",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Priya",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Dharaneesh",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Naveen",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Priya",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+    {
+        name:"Priya",
+        image:"https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg"
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const con = document.getElementById('container');
+for(let i=0;i<myData.length;i++)
+{
+    con.innerHTML+=`
+    <div class='card'>
+    <img src=${myData[i].image} height="100px" />
+      <p>${myData[i].name}</p>
+    </div>
+    `
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch('https://fakestoreapi.com/products')
+.then(res=>
+   res.json()//converts json to js object
+)
+.then((data)=>{
+    for(let i=0;i<data.length;i++)
+{
+    con.innerHTML+=`
+    <div class='card'>
+    <img src=${data[i].image} height="100px" />
+    <p>${data[i].id}</p>
+    </div>
+    `
+}    
+})
+
+.catch((err)=>{
+    alert("error occured")
+})
+
+
+
+
